e2860ff3d47ca388e08ce1505b081983a76f69cf
Felipe de Oliveira <Felipe.GameplayDev@outlook.com>
2022-06-04 09:38:17 -0300

    Conditional
    Exercício 1: Dois inteiros

---
+++ 1-conditional/ex1-two-integers/Program.cs
@@ -0,0 +1,17 @@
+﻿/*
+Objetivo: Escrever um programa C# Sharp para aceitar dois inteiros e verificar se eles são iguais ou não.
+Output: 5 e 5 são iguais!
+*/
+
+//entrada
+Console.Write("1° número: "); // 5;
+int firstNumber = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("2° número: "); // 5;
+int secondNumber = Convert.ToInt32(Console.ReadLine());
+
+//logica
+if ( firstNumber == secondNumber)
+    Console.WriteLine($"{firstNumber} e {secondNumber} são iguais!");
+else
+    Console.WriteLine($"{firstNumber} e {secondNumber} não são iguais!");
