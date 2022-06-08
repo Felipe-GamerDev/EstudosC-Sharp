9c4f995437ddbeec16e087ffc961861a160ff7f7
Felipe de Oliveira <Felipe.GameplayDev@outlook.com>
2022-06-08 17:58:47 -0300

    For Loop
    Programa de tabela para multiplicação de um número - concluído.

---
+++ 2-for-loop/ex2-multiplication-table/Program.cs
@@ -0,0 +1,17 @@
+﻿/*
+Objetivo: Escreva um programa em C# para exibir a tabela de multiplicação de um determinado inteiro.
+*/
+
+//entrada
+int number, repeat = 0, multiplyResult, limitRepeat = 10;
+Console.WriteLine("===== Tabela de Multiplicação =====");
+
+Console.Write("Insira o número: ");
+number = Convert.ToInt32(Console.ReadLine());
+
+//lógica
+for (repeat = 1; repeat <= limitRepeat; repeat++)
+{
+    multiplyResult = number * repeat;
+    Console.WriteLine($"{number} X {repeat} = {multiplyResult}");
+}
